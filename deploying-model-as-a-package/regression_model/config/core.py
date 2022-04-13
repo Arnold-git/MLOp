a546e773bcf4d665f467e0642adbba203671b54b
Arnold-git <arnoldighiwiyisi@gmail.com>
2022-04-13 17:24:12 +0100

    deploying model as a package  :technologist:

--- deploying-model-as-a-package/regression_model/config/core.py
+++ deploying-model-as-a-package/regression_model/config/core.py
@@ -6,3 +6,62 @@ from pydantic import BaseModel
 from strictyaml import YAML, load
 
 import regression_model
+
+
+# Project Directories
+PACKAGE_ROOT = Path(regression_model.__file__).resolve().parent
+ROOT = PACKAGE_ROOT.parent
+CONFIG_FILE_PATH = PACKAGE_ROOT / "config.yml"
+DATASET_DIR = PACKAGE_ROOT / "datasets"
+TRAINED_MODEL_DIR = PACKAGE_ROOT / "trained_models"
+
+
+class AppConfig(BaseModel):
+    """
+    Application level config
+    """
+
+    package_name: str
+    training_data_file: str
+    test_data_file: str
+    pipeline_save_file: str
+
+
+
+
+class ModelConfig(BaseModel):
+    """
+    All configurations relevant to model
+    training and feature engineering
+    """
+
+    target: str
+    variables_to_rename: Dict
+    features: List[str]
+    test_size: float
+    random_state: int
+    alpha: float
+    categorical_vars_with_na_frequent: List[str]
+    categorical_vars_with_na_missing: List[str]
+    numerical_vars_with_na: List[str]
+    temporal_vars: List[str]
+    ref_var: str
+    numericals_log_vars: Sequence[str]
+    binarize_vars: Sequence[str]
+    qual_vars: List[str]
+    exposure_vars: List[str]
+    finish_vars: List[str]
+    garage_vars: List[str]
+    categorical_vars: Sequence[str]
+    qual_mappings: Dict[str, int]
+    exposure_mappings: Dict[str, int]
+    garage_mappings: Dict[str, int]
+    finish_mappings: Dict[str, int]
+
+
+
+class Config(BaseModel):
+    """Master config object"""
+
+    app_config: AppConfig
+    model_config: ModelConfig
