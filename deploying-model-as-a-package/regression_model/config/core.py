e2dc7c3b55efc1f18ffc1a8dc07f98fc6f81878e
Arnold-git <arnoldighiwiyisi@gmail.com>
2022-04-14 15:28:22 +0100

    deploying model as a package  :technologist:

--- deploying-model-as-a-package/regression_model/config/core.py
+++ deploying-model-as-a-package/regression_model/config/core.py
@@ -1,3 +1,4 @@
+from distutils.command.config import config
 import imp
 from pathlib import Path
 from typing import Dict, List, Sequence
@@ -72,3 +73,32 @@ def find_config_file() -> Path:
     if CONFIG_FILE_PATH.is_file():
         return CONFIG_FILE_PATH
     raise Exception(f'Config not found at {CONFIG_FILE_PATH!r}')
+
+
+def fetch_config_from_yaml(cfg_path: Path = None) -> YAML:
+    """Parse YAML containing the package configuration"""
+
+    if not cfg_path:
+        cfg_path = find_config_file()
+
+    if cfg_path:
+        with open(cfg_path, "r") as conf_file:
+            parse_config = load(conf_file.read())
+            return parse_config
+    raise OSError(f"Did not find config file at path: {cfg_path}")
+
+
+def create_and_validate_config(parsed_config: YAML = None) -> Config:
+    """Run validation on config values"""
+    if parsed_config is None:
+        parsed_config = fetch_config_from_yaml()
+
+    _config = Config(
+        app_config = AppConfig(**parsed_config.data),
+        model_config = ModelConfig(**parsed_config.data),
+    )
+
+    return _config
+
+
+config = create_and_validate_config()
