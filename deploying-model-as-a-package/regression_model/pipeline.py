6221df960f45371f1790d589e745bda3f9b04722
Arnold-git <arnoldighiwiyisi@gmail.com>
2022-04-18 21:02:07 +0100

    deploying model as a package  :technologist:

--- deploying-model-as-a-package/regression_model/pipeline.py
+++ deploying-model-as-a-package/regression_model/pipeline.py
@@ -37,14 +37,104 @@ price_pipe = Pipeline(
             CategoricalImputer(
                 imputation_method="frequent",
                 variables=config.model_config.categorical_vars_with_na_frequent,
-            )
+            ),
         ),
+
+        # add missing indicator
         (
             "missing_indicator",
+            AddMissingIndicator(variables=config.model_config.numerical_vars_with_na),
+        ),
+
+        (
+            "mean_imputation",
             MeanMedianImputer(
                 imputation_method="mean",
-                variables=config.model_config.numerical_vars_with_na
-            )
-        )
+                variables=config.model_config.numerical_vars_with_na,
+            ),
+        ),
+        # == TEMPORAL VARIABLES ====
+        (
+            "elapsed_time",
+            pp.TemporalVariableTransformer(
+                variables=config.model_config.temporal_vars,
+                reference_variable=config.model_config.ref_var
+            ),
+        ),
+
+        (
+            "drop_features",
+            DropFeatures(features_to_drop=[config.model_config.ref_var]),
+        ),
+
+        ("log", LogTransformer(variables=config.model_config.numericals_log_vars)),
+
+        (
+            "binarizer",
+            SklearnTransformerWrapper(
+                transformer=Binarizer(threshold=0),
+                variables=config.model_config.binarize_vars,
+            ),
+        ),
+        # === mappers ===
+        (
+            "mapper_qual",
+            pp.Mapper(
+                variables=config.model_config.qual_mappings,
+                mappings=config.model_config.qual_mappings,
+            ),
+        ),
+
+        (
+            "mapper_exposure",
+            pp.Mapper(
+                variables=config.model_config.exposure_vars,
+                mappings=config.model_config.exposure_mappings
+            ),
+        ),
+
+        (
+            "mapper_finish",
+            pp.Mapper(
+                variables=config.model_config.finish_vars,
+                mappings=config.model_config.finish_mappings
+            ),
+        ),
+
+        (
+            "mapper_garage",
+            pp.Mapper(
+                variables=config.model_config.garage_vars,
+                mappings=config.model_config.garage_mappings,
+            ),
+        ),
+        # == CATEGORICAL ENCODING
+
+        (
+            "rare_label_encoder",
+            RareLabelEncoder(
+                tol=0.01, n_categories=1, variables=config.model_config.categorical_vars
+            ),
+        ),
+
+        (
+            "categorical_encoder",
+            OrdinalEncoder(
+                encoding_method="ordered",
+                variables=config.model_config.categorical_vars,
+            ),
+        ),
+
+        (
+            "scaler", MinMaxScaler()
+        ),
+        (
+            "Lasso",
+            Lasso(
+                alpha=config.model_config.alpha,
+                random_state=config.model_config.random_state
+            ),
+        ),
+
     ]
 )
