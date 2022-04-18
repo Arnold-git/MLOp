3064643bd619183466fa9780a06200b1c2624e6b
Arnold-git <arnoldighiwiyisi@gmail.com>
2022-04-18 20:18:44 +0100

    deploying model as a package  :technologist:

--- deploying-model-as-a-package/regression_model/pipeline.py
+++ deploying-model-as-a-package/regression_model/pipeline.py
@@ -12,9 +12,39 @@ from feature_engine.imputation import(
 from feature_engine.selection import DropFeatures
 from feature_engine.transformation import LogTransformer
 from feature_engine.wrappers import SklearnTransformerWrapper
+from numpy import var
 from sklearn.linear_model import Lasso
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import Binarizer, MinMaxScaler
 
 from regression_model.config.core import config
 from regression_model.processing import features as pp
+
+
+price_pipe = Pipeline(
+    [
+
+        (
+            "missing_imputation",
+            CategoricalImputer(
+                imputation_method="missing",
+                variables=config.model_config.categorical_vars_with_na_missing
+            ),
+        ),
+
+        (
+            "frequent_imputation",
+            CategoricalImputer(
+                imputation_method="frequent",
+                variables=config.model_config.categorical_vars_with_na_frequent,
+            )
+        ),
+        (
+            "missing_indicator",
+            MeanMedianImputer(
+                imputation_method="mean",
+                variables=config.model_config.numerical_vars_with_na
+            )
+        )
+    ]
+)
