8624b5348efe30e158e999b3ea02ed40df0556da
Arnold-git <arnoldighiwiyisi@gmail.com>
2022-04-22 20:52:25 +0100

    added data manager script  :technologist:

---
+++ deploying-model-as-a-package/regression_model/train_pipeline.py
@@ -0,0 +1,31 @@
+from distutils.command.config import config
+from tkinter import N
+from tkinter.messagebox import NO
+import numpy as np
+from config.core import config
+from pipeline import price_pipe
+from processing.data_manager import load_dataset, save_pipeline
+from sklearn.model_selection import train_test_split
+
+
+def run_training() -> None:
+    """Train the model"""
+
+    data = load_dataset(file_name=config.app_config.training_data_file)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        data[config.model_config.features],
+        data[config.model_config.target],
+        test_size=config.model_config.test_size,
+
+        random_state=config.model_config.random_state,
+    )
+    y_train = np.log(y_train)
+
+    price_pipe.fit(X_train, y_train)
+
+    save_pipeline(pipeline_to_persist=price_pipe)
+
+
+if __name__ == "__main__":
+    run_training()
