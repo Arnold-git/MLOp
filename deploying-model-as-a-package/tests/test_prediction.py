17bac9b0bb586aa959cf2d8424e9e268daec40db
Arnold-git <arnoldighiwiyisi@gmail.com>
2022-05-16 18:52:46 +0100

    added test scripts :art:

--- deploying-model-as-a-package/tests/test_prediction.py
+++ deploying-model-as-a-package/tests/test_prediction.py
@@ -1,4 +1,5 @@
-import math 
+import math
+from unittest import result 
 
 import numpy as np
 
@@ -7,4 +8,14 @@ from regression_model.predict import make_prediction
 
 def test_make_prediction(sample_input_data):
 
-    pass
+    expected_first_prediction_value = 113422
+    expected_no_predictions = 1449
+
+    result = make_prediction(input_data=sample_input_data)
+
+    predictions = result.get('predictions')
+    assert isinstance(predictions, list)
+    assert isinstance(predictions[0], np.float64)
+    assert result.get('errors') is None
+    assert len(predictions) == expected_no_predictions
+    assert math.isclose(predictions[0], expected_first_prediction_value, abs_tol=100)
